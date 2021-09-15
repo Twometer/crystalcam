f3c1e0fd7b483135de576795e6fdc8d3f4b44a59
Twometer <twometer@outlook.de>
2021-09-15 17:13:34 +0200

    Create basic project

---
+++ app/src/main/java/de/twometer/crystalcam/MainActivity.kt
@@ -0,0 +1,28 @@
+package de.twometer.crystalcam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        hideSystemUi()
+    }
+
+    fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(
+            window,
+            window.decorView
+        ).let {
+            it.hide(WindowInsetsCompat.Type.systemBars())
+            it.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+}
